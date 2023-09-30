90b5821dba6c8fd9b93693d0ecc4834b5c231c3e
SakthiVel08072004 <ksk25678@gmail.com>
2023-09-30 12:24:31 +0530

    webapp

---
+++ Day7.js
@@ -0,0 +1,5 @@
+
+ function fun(){
+     var number=document.getElementById('number').value;
+    alert(number*number*number);
+ }
---
+++ tech-solution/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<div class="bg">  
+</div>
+</div>
+  );
+}
+
+export default App;
